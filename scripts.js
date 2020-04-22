084e629ba5a25849586aee3f036521b4cfbb9fd6
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-04-22 10:48:16 -0500

    sum die button console logs innerHTML on reroll and after adding new dice

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
 const newDie = document.getElementById('new');
 const reroll = document.getElementById('reroll');
+const sum = document.getElementById('sum');
 const section = document.getElementById('roll-section');
 let num
-let dice = document.getElementsByClassName('div');
-let dieArray = Array.from(dice);
-
+let diceArray = [];
 
 class Die {
     constructor() {
@@ -13,42 +12,41 @@ class Die {
         this.value = document.createTextNode(num);
         this.div.className = ('dice');
         this.render();
-        this.div.addEventListener("click", () => this.div.style.backgroundColor = this.randomColor());
-
     }
 
     render() {
         section.appendChild(this.div);
         this.div.id = num;
         this.div.appendChild(this.value);
-        dieArray.push(this.div);
+        diceArray.push(this.div);
     }
 
     roll() {
         num = Math.ceil(Math.random() * 6);
     }
-
-    randomColor() {
-        let r = Math.floor(Math.random() * 256);
-        let g = Math.floor(Math.random() * 256);
-        let b = Math.floor(Math.random() * 256);
-        return `rgb(${r}, ${g}, ${b})`;
-    }
 }
 
 // add die on button click
-newDie.addEventListener("click", function () {
+newDie.addEventListener("click", function() {
     new Die();
 })
 
 // reroll on button click
-reroll.addEventListener("click", function () {
-
-    dieArray.forEach(element => {
+reroll.addEventListener("click", function() {
+    diceArray.forEach(element => {
         element.textContent = null;
         num = Math.ceil(Math.random() * 6);
         dieVal = document.createTextNode(num);
         newVal = element.appendChild(dieVal);
         element.id = num;
     });
+})
+
+//sum dice values on button click
+sum.addEventListener('click', function() {
+    diceArray.forEach(element => {
+        let diceVal = element.innerHTML;
+        let diceValArray = Array.from(diceVal);
+        console.log(diceVal);
+    })
 })
